6133faab1980e0dfbace14c96c35a44760f5ce74
wichodeobras <luisenriquesantiagoromano@gmail.com>
2025-03-23 21:38:51 -0600

    Agrega Tabla CE y CF

--- backend/mybackend/mybackend/urls.py
+++ backend/mybackend/mybackend/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.http import HttpResponse  # Agregar esta importación
 from .views import hello_world, ir_list, ir_detail, ir_create, ir_update, ir_delete
+from .views import hello_world, hp_list, hp_detail, hp_create, hp_update, hp_delete
 
 def home(request):
     return HttpResponse("¡Backend funcionando en Render!")
@@ -26,10 +27,19 @@ urlpatterns = [
     path('', home, name='home'),  # Nueva ruta para "/"
     path('api/message/', hello_world),
     path('admin/', admin.site.urls),
+
+    #perfiles IR
     path('ir/', ir_list, name='ir_list'),
     path('ir/<int:ir_id>/', ir_detail, name='ir_detail'),
     path('ir/create/', ir_create, name='ir_create'),
     path('ir/<int:ir_id>/update/', ir_update, name='ir_update'),
     path('ir/<int:ir_id>/delete/', ir_delete, name='ir_delete'),
+
+    #perfiles HP
+    path('hp/', hp_list, name='hp_list'),
+    path('hp/<int:hp_id>/', hp_detail, name='hp_detail'),
+    path('hp/create/', hp_create, name='hp_create'),
+    path('hp/<int:hp_id>/update/', hp_update, name='hp_update'),
+    path('hp/<int:hp_id>/delete/', hp_delete, name='hp_delete'),
 ]
 
--- backend/mybackend/mybackend/views.py
+++ backend/mybackend/mybackend/views.py
@@ -4,11 +4,12 @@ from django.views.decorators.http import require_http_methods
 import json
 
 from .models import IR
+from .models import HP
 
 def hello_world(request):
     return JsonResponse({"message": "¡Hola desde Django!"})
 
-
+#CRUD IR
 # Listar todos los registros
 @require_http_methods(["GET"])
 def ir_list(request):
@@ -84,5 +85,87 @@ def ir_delete(request, ir_id):
         return JsonResponse({"message": "Eliminado correctamente"})
     except IR.DoesNotExist:
         return JsonResponse({"error": "IR no encontrado"}, status=404)
+
+
+#CRUD HP
+# Listar todos los registros
+@require_http_methods(["GET"])
+def hp_list(request):
+    data = list(HP.objects.values())
+    return JsonResponse(data, safe=False)
+
+
+# Obtener un solo registro por ID
+@require_http_methods(["GET"])
+def hp_detail(request, hp_id):
+    try:
+        hp = HP.objects.get(id=hp_id)
+        data = {
+            "id": hp.id,
+            "designacion_mm": hp.designacion_mm,
+            "designacion_pulg": hp.designacion_pulg,
+            "d": hp.d,
+            "T": hp.T,
+            "tw": hp.tw,
+            "bf": hp.bf,
+            "tf": hp.tf,
+            "k": hp.k,
+            "k1": hp.k1,
+            "peso": hp.peso,
+            "area": hp.area,
+            "b2ft": hp.b2ft,
+            "htw": hp.htw,
+            "ix": hp.ix,
+            "zx": hp.zx,
+            "sx": hp.sx,
+            "rx": hp.rx,
+            "iy": hp.iy,
+            "zy": hp.zy,
+            "sy": hp.sy,
+            "ry": hp.ry,
+            "j": hp.j,
+            "cw": hp.cw,
+        }
+        return JsonResponse(data)
+    except HP.DoesNotExist:
+        return JsonResponse({"error": "HP no encontrado"}, status=404)
+
+# Crear nuevo registro
+@csrf_exempt
+@require_http_methods(["POST"])
+def hp_create(request):
+    try:
+        data = json.loads(request.body)
+        hp = HP.objects.create(**data)
+        return JsonResponse({"id": hp.id}, status=201)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=400)
+
+# Actualizar registro existente
+@csrf_exempt
+@require_http_methods(["PUT"])
+def hp_update(request, hp_id):
+    try:
+        hp = HP.objects.get(id=hp_id)
+        data = json.loads(request.body)
+        for field, value in data.items():
+            setattr(hp, field, value)
+        hp.save()
+        return JsonResponse({"message": "Actualizado correctamente"})
+    except HP.DoesNotExist:
+        return JsonResponse({"error": "HP no encontrado"}, status=404)
+    except Exception as e:
+        return JsonResponse({"error": str(e)}, status=400)
+
+# Eliminar registro
+@csrf_exempt
+@require_http_methods(["DELETE"])
+def hp_delete(request, hp_id):
+    try:
+        hp = HP.objects.get(id=hp_id)
+        hp.delete()
+        return JsonResponse({"message": "Eliminado correctamente"})
+    except HP.DoesNotExist:
+        return JsonResponse({"error": "HP no encontrado"}, status=404)
 
 
